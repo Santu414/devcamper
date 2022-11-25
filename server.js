db2f68a59840b270b0954d20219dd01453827a1c
Santosh Reddy <snagankera@gmail.com>
2022-11-25 16:39:09 +0530

    Connecting to the database using mongoose

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//connection to DB
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
