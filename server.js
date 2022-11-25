41763bd6e2f12eb06f5f50089a6808ce31887f29
Santosh Reddy <snagankera@gmail.com>
2022-11-25 18:25:27 +0530

    Creating routes and response

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ const app = express();
 //connection to DB
 connectDB();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  console.log("Hello");
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
+});
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
