4646bc315f15d7a5c807954021e468a486adfdf1
Santosh Reddy <snagankera@gmail.com>
2022-11-26 00:25:39 +0530

    Introduction to middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
+const logger = require("./middleware/logger");
 
 //Route files
 const bootcamps = require("./routes/bootcamps");
@@ -9,6 +11,10 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+//Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 //connection to DB
 connectDB();
